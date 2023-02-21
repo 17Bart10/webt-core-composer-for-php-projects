ba1fdff7d46cf2bb86fe16edcef5ccd05e348804
Nico Zach <8157@htl.rennweg.at>
2023-02-21 19:04:39 +0100

    User Story 5

--- index.php
+++ index.php
@@ -1,29 +1,66 @@
 <?php
 namespace Htlw3r\PaaQrCode;
-require('vendor/autoload.php');
+?>
 
-use Endroid\QrCode\Builder\Builder;
-use Endroid\QrCode\Encoding\Encoding;
-use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
-use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
-use Endroid\QrCode\Label\Font\NotoSans;
-use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
-use Endroid\QrCode\Writer\PngWriter;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PA-QR-CodeForm</title>
+</head>
+<body>
+        
+    <form action="." method="post">    
+        <label for="name">Owner's Name:</label>
+        <input type="text" name="name" placeholder="Maxine Mustermann"><br>
+            
+        <label for="phone">Phone Number:</label>
+        <input type="tel" name="phone" placeholder="+4312345678" required><br>
+            
+        <input type="submit" value="Create QR-Code">
+    </form>
 
-$result = Builder::create()
-    ->writer(new PngWriter())
-    ->writerOptions([])
-    ->data('tel:+4312233444')
-    ->encoding(new Encoding('UTF-8'))
-    ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
-    ->size(300)
-    ->margin(10)
-    ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
-    ->labelText('Phone Number')
-    ->labelFont(new NotoSans(20))
-    ->labelAlignment(new LabelAlignmentCenter())
-    ->validateResult(false)
-    ->build();
 
-header('Content-Type: '.$result->getMimeType());
-echo $result->getString();
+    <?php     
+        require('vendor/autoload.php');
+        use Endroid\QrCode\Builder\Builder;
+        use Endroid\QrCode\Encoding\Encoding;
+        use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+        use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+        use Endroid\QrCode\Label\Font\NotoSans;
+        use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+        use Endroid\QrCode\Writer\PngWriter;
+
+        // if(isset($_POST["name"]) && isset($_POST["phone"])){
+        //     echo "<br>";
+        //     echo "Owner's Name: " . $_POST["name"];
+        //         echo "<br>";
+        //         echo "Phone Number: " . $_POST["phone"]; 
+        //     } 
+
+        
+        if(isset($_POST["name"]) && isset($_POST["phone"])){
+            $result = Builder::create()
+                ->writer(new PngWriter())
+                ->writerOptions([])
+                ->data('tel:'.$_POST['phone'])
+                ->encoding(new Encoding('UTF-8'))
+                ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+                ->size(600)
+                ->margin(10)
+                ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+                ->labelText($_POST['name'])
+                ->labelFont(new NotoSans(20))
+                ->labelAlignment(new LabelAlignmentCenter())
+                ->validateResult(false)
+                ->build();
+
+            $dataUri = $result->getDataUri();            
+            echo '<img src="'. $dataUri .'"alt="Dynamc Generated QR Code">';
+        }
+    ?>
+        
+
+
+</body>
+</html>
